e81c2cb1a577b3b6fc0d927c28d11203f237b397
Rishabh <rishabhh.singh.rs@gmail.com>
2023-08-19 14:29:18 +0530

    added known hosts file

--- part-2-scripts/deploy.sh
+++ part-2-scripts/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.0/bin
+export PATH=$PATH:/home/ubuntu/.nvm/versions/node/v20.5.1/bin
 
 cd Week-5-with-axios-and-backend
 git pull origin master
